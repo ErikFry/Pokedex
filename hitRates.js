bc7718bff7870dd9b71780649e66b97714b00558
ErikFry <erifry88@gmail.com>
2025-03-31 21:32:41 +0200

    Fiksa masse greier

---
+++ hitRates.js
@@ -0,0 +1,11 @@
+let hitRates = [
+    //{name:"Common", chance: 10}, Not a chance cause every card has x common
+    //{name:"Uncommon", chance: 10}, Not a chance cause every pack has 
+    {name:"Double rare", chance: 0.1328},
+    {name:"Ultra Rare", chance: 0.0644},
+    {name:"FoilEnergy", chance: 0.2483},
+    {name:"Illustration rare", chance: 0.0850},
+    {name:"Special illustration rare", chance: 0.0311},
+    {name:"Hyper rare", chance: 0.0194},    
+]
+
